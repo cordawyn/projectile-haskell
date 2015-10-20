4b0f0c805ee6cfca7e6fc7cfc99febd7cea0f169
Slava Kravchenko <slava.kravchenko@gmail.com>
2015-10-20 20:07:31 +0200

    smarter handling of projectile-test-suffix-function

--- projectile-haskell.el
+++ projectile-haskell.el
@@ -1,7 +1,10 @@
-;; Identify a Haskell project root by the location of its "Setup.hs" file
-(add-to-list 'projectile-project-root-files "Setup.hs")
-(setq projectile-test-suffix-function
-      (lambda (project-type)
-	(if (eq 'haskell-cabal project-type)
-	    "Test"
-	  (projectile-test-suffix project-type))))
+(add-hook 'projectile-mode-hook
+          (lambda ()
+            ;; Identify a Haskell project root by the location of its "Setup.hs" file
+            (add-to-list 'projectile-project-root-files "Setup.hs")
+            (setq projectile-test-suffix-function
+                  (lambda (project-type)
+                    (if (member project-type '(haskell-cabal haskell-stack))
+                        "Test"
+                      ;; call the original implementation if it's not a haskell project
+                      (funcall (get 'projectile-test-suffix-function 'standard-value) project-type))))))
